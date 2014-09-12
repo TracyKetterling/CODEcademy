8d0568d663f4d196f1c7c5f95f027305d3c47276
TracyKetterling <hunkpapa.lakota@gmail.com>
2014-09-12 11:11:19 -0500

    wrote the code to have the tax and tip included in the total meal cost and print to screen.

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,2 +1,11 @@
-# Assign the variable meal the value 44.50 on line 3!
-
+"""This is an application that calculates the cost of a meal, tax and tip."""
+# assign values to variables of meal, tax and tip
+meal = 44.50
+tax = 0.0675
+tip = 15.0/100
+# we are reassigning the variable meal to include the cost of the meal plus tax
+meal = meal + meal * tax
+# we are assigning the variable total to include meal, tip and tax
+total = meal + meal * tip
+# line 11 will print the above information into the console
+print("%.2f" % total)
